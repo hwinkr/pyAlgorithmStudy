1f19b1f953dfe131e4778d7466b83260a52823e1
woong <dnddl8280@naver.com>
2022-09-29 14:37:12 +0900

    [BOJ] 신입사원 실버1

--- woong/greedy/1946.py
+++ woong/greedy/1946.py
@@ -1,53 +1,23 @@
-# import sys
-# input = sys.stdin.readline
-
-# test = int(input())
-# pass_lst = []
-# for _ in range(test):
-#     n = int(input())
-#     lst = [list(map(int, input().split())) for _ in range(n)]
-
-#     lst.sort(key=lambda x: x[0])
-
-#     cnt = 0
-#     interview = n + 1
-
-#     for i in range(n):
-#         if lst[i][1] < interview:
-#             cnt += 1
-#             interview = lst[i][1]
-
-#     pass_lst.append(cnt)
-
-# for pass_cnt in pass_lst:
-#     print(pass_cnt)
-
-
 import sys
 input = sys.stdin.readline
 
+test = int(input())
+pass_lst = []
+for _ in range(test):
+    n = int(input())
+    lst = [list(map(int, input().split())) for _ in range(n)]
 
-def solution():
-    test = int(input())
-    pass_lst = []
-    for _ in range(test):
-        n = int(input())
-        ranks = [0] * (n + 1)
-        for _ in range(n):
-            s1, s2 = map(int, input().split())
-            ranks[s1] = s2
+    lst.sort(key=lambda x: x[0])
 
-        cnt = 1
-        curr = ranks[1]
+    cnt = 0
+    interview = n + 1
 
-        for rank in ranks[2:]:
-            if rank < curr:
-                cnt += 1
-                curr = rank
-        pass_lst.append(str(cnt))
+    for i in range(n):
+        if lst[i][1] < interview:
+            cnt += 1
+            interview = lst[i][1]
 
-    print('\n'.join(pass_lst))
+    pass_lst.append(cnt)
 
-
-if __name__ == "__main__":
-    solution()
+for pass_cnt in pass_lst:
+    print(pass_cnt)
