0e36ee0972a10a9e7ece215bf1eefad3f6051696
woong <dnddl8280@naver.com>
2022-09-29 14:27:31 +0900

    [BOJ] 4796 브론즈 1

---
+++ woong/greedy/4796.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+i = 1
+while True:
+    l, p, v = map(int, input().split())
+    if l == 0 and p == 0 and v == 0:
+        break
+    day = 0
+    day += (v // p) * l
+    if v % p > l:
+        day += l
+    else:
+        day += v % p
+
+    print(f"Case {i}: {day}")
+    i += 1
