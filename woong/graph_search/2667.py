bfb8135aa5596ea80507f2a09cae414cd06cf5c9
woong <dnddl8280@naver.com>
2022-11-01 12:16:20 +0900

    [BOJ] 단지번호 붙이기 실버1

--- woong/graph_search/2667.py
+++ woong/graph_search/2667.py
@@ -28,6 +28,7 @@ def bfs(i, j):
 
 if __name__ == "__main__":
     n = int(input())
+    # * 공백이 없는 상태로 입력을 받는다면 rstrip 를 사용해야 한다. 이 함수는 문자열의 공백을 제거해준다
     graph = [list(map(int, input().rstrip())) for _ in range(n)]
 
     dx = [0, 0, -1, 1]
@@ -39,6 +40,8 @@ if __name__ == "__main__":
     for i in range(n):
         for j in range(n):
             if graph[i][j] == 1:
+                # * 하나의 단지에 속하는 집을 발견하면 bfs 탐색을 시작해준다.
+                # * 개인적인 생각 : dfs 는 재귀함수를 계속 호출하기 떄문에 return 값을 처리하기 힘들다, 탐색만 하는 경우가 아닌 함수가 어떤 값을 반환해줘야 하는 경우라면 bfs 를 사용하는 것이 더 편함
                 group_cnt += 1
                 house_cnt_list.append(bfs(i, j))
 
