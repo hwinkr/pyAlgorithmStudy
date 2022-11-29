62736006833099704d6192f9c7ecf8f252838eab
juone00 <juwon0208@naver.com>
2022-11-30 02:34:27 +0900

    [BOJ] 3020 골드5

---
+++ juwon/binary_search/3020.py
@@ -0,0 +1,16 @@
+# -- 수정중 --
+N, H = map(int, input().split())
+obstacle = []
+for _ in range(N):
+    obstacle.append(int(input()))
+    
+start = 0
+end = H
+
+result = 0
+while start <= end:
+    total = 0
+    mid = (start+end) // 2
+    
+    for x in obstacle:
+        
